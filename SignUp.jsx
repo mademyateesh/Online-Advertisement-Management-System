497bef922f9fe12163f803d556cefa1a75756839
mademyateesh <151800704+mademyateesh@users.noreply.github.com>
2024-03-10 11:03:04 +0530

    Add files via upload

---
+++ SignUp.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      console.log(data);
+
+      if (data.success === false) {
+        setError(data.message);
+        return;
+      }
+
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+        <div className='relative'>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            placeholder='password'
+            className='border p-3 rounded-lg w-full pr-10' // Adjusted width and added paddingRight
+            id='password'
+            onChange={handleChange}
+          />
+          <button
+            type='button'
+            className='absolute top-1/2 right-2 transform -translate-y-1/2 cursor-pointer bg-blue-500 text-white p-2 rounded-md'
+            onClick={handleTogglePassword}
+          >
+            {showPassword ? 'Hide' : 'Show'}
+          </button>
+        </div>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Sign Up'}
+        </button>
+        <OAuth />
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
+  );
+}
+
+export default SignUp;
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBVdRNLcxz-IwG6xDwBbgXBNgnB2lq2gs",
+  authDomain: "real-estate-680f0.firebaseapp.com",
+  projectId: "real-estate-680f0",
+  storageBucket: "real-estate-680f0.appspot.com",
+  messagingSenderId: "918832305900",
+  appId: "1:918832305900:web:be2abc98168f974cd477c9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
